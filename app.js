fdcc5e28e1ffae3061b70428023813045ca47019
JaKaczocha <jakub.kaczocha44295@gmail.com>
2023-11-21 22:40:19 +0100

    lab5

--- app.js
+++ app.js
@@ -1,53 +1,56 @@
 const express = require('express');
 const config = require('./config').config; // dodając tę linię, trzeba pamiętać o stworzeniu pliku config.js
-const service = require('./service');
+
+const path = require('path');
 
 const app = express();
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.listen(config.port, function () {
+    console.info(`Server is running at port ${config.port}`);
+});
 
 
 
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
-const temp = `Czas,Temperatura
-2023-11-09 10:00:00,10
-2023-11-09 11:00:00,13
-2023-11-09 12:00:00,16
-2023-11-09 13:00:00,18
-2023-11-09 14:00:00,19
-2023-11-09 15:00:00,19
-2023-11-09 16:00:00,17`;
-
-const products = [
-    { name: 'Laptop', price: 1000 },
-    { name: 'Smartphone', price: 500 },
-    { name: 'Tablet', price: 300 }
-];
-
-
-const dataChart2 = service.convertData(temp);
-const chart2 = JSON.stringify(dataChart2);
-
-
-
-
-
-let chart1 = {"type":"line","data":{"labels":["January","February","March","April","May","June"],
-        "datasets":[{"label":"My First dataset","backgroundColor":"rgb(255, 99, 132)",
-            "borderColor":"rgb(255, 99, 132)","data":[0,10,5,2,20,30,45]}]}, "options":{}};
 
 app.get('/', (request, response) => {
-    response.render(__dirname + '/index.html', {subject: 'Technologie webowe w aplikacjach Internetu',
-        chart1: JSON.stringify(chart1),chart2:chart2,products:products})
+    response.render(__dirname + '/index.html', {subject: 'Technologie webowe w aplikacjach Internetu', result: ' '})
 })
+app.get('/template/:variant/:a/:b', (request, response) => {
+    const variant = request.params.variant;
+    const a = Number(request.params.a);
+    const b = Number(request.params.b);
+    let result;
 
+    switch(variant) {
+        case 'sum':
+            result = a + b;
+            break;
+        case 'sub':
+            result = a - b;
+            break;
+        case 'multiply':
+            result = a * b;
+            break;
+        case 'divide':
+            if(b !== 0) {
+                result = a / b;
+            } else {
+                result = 'Error: Division by zero';
+            }
+            break;
+        default:
+            result = 'Error: Invalid operation';
+    }
 
-
-
-
-app.listen(config.port, function () {
-    console.info(`Server is running at port 3000`);
+    response.render(__dirname + '/index.html', {result: result})
 });
 
 
